dfb47d29ee654db403ede7a9e85a999fba664d08
shallabh kohal <shallabh.rajput9@gmail.com>
2024-06-19 13:09:39 +0530

    new update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./routes/user")
+require("./db/conn")
 
 const app = express();
 
@@ -8,5 +9,5 @@ app.use(express.json());
 app.use(router)
 
 app.listen(process.env.port || 3500 ,()=>{
-    console.log("connection succesfull")
+    console.log("connection succesfull to port ")
 })
